67e9d689678d2b98e174493c612c74159bec086e
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2023-03-29 21:42:05 -0700

    Add createWarning function and slideDown() method

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -36,27 +36,38 @@
 
 //Document Ready shorthand
 $(() => {
-  //Store tweet-container section in variable
+  //Grab length-warning section from the DOM
+  const $lengthWarning = $("#length-warning");
+  //Grab tweet-container section from the DOM
   const $tweetContainer = $("#tweet-container");
+  //Grab the form from the DOM
+  const $form = $('#new-tweet-form');
+
   //Create tweet using tweet template
   const createTweet = (tweet) => {
+    //Prevent XSS and safely render insecure text
+    const escape = function(str) {
+      let div = document.createElement("div");
+      div.appendChild(document.createTextNode(str));
+      return div.innerHTML;
+    };
     const $tweet = $(`
     <article class="tweet">
         <header class="tweet-header">
           <div class="tweet-header-fullname">
-            <img src="${tweet.user.avatars}">
-            <p>${tweet.user.name}</p>
+            <img src="${escape(tweet.user.avatars)}">
+            <p>${escape(tweet.user.name)}</p>
           </div>
           <div class="tweet-header-username">
-            <p>${tweet.user.handle}</p>
+            <p>${escape(tweet.user.handle)}</p>
           </div>
         </header>
         <article class="tweet-text">
-          <p>${tweet.content.text}</p>
+          <p>${escape(tweet.content.text)}</p>
         </article>
         <footer class="tweet-footer">
           <div class="tweet-footer-days">
-            <p>${timeago.format(tweet.created_at)}</p>
+            <p>${escape(timeago.format(tweet.created_at))}</p>
           </div>
           <div class="tweet-footer-icons">
             <i class="fa-solid fa-flag"></i>
@@ -78,9 +89,6 @@ $(() => {
     }
   };
 
-  // Render the tweets on initial load
-  // renderTweets(tweets);
-
   // Load tweets from server
   const loadTweets = function() {
     $.ajax({
@@ -97,25 +105,46 @@ $(() => {
   //Load tweets on initial load
   loadTweets();
 
-  //Grab the form from the DOM
-  const $form = $('#new-tweet-form');
+  //Renders warning 
+  const createWarning = function(warning) {
+    const $warning = $(`
+    <article class="warning">
+      <i class="fa-solid fa-triangle-exclamation fa-2xl"></i>
+      <p>${warning}</p>
+      <i class="fa-solid fa-triangle-exclamation fa-2xl"></i>
+    </article>
+    `);
+    return $warning;
+  };
 
   $form.on('submit', (event) => {
     // Stop the browser from refreshing the page
     event.preventDefault();
-  
+
+    //Validate form by checking length, display warning if not valid
     const $formLength = $('textarea').val().length;
     console.log($formLength);
 
     if ($formLength > 140) {
-      alert('Maximum character length exceeded');
+      const excessWarning = 'Please shorten your tweet to less than 140 characters';
+      const $warning = createWarning(excessWarning);
+
+      $lengthWarning.html($warning);
+      $lengthWarning.slideDown(1000);
       return;
     }
 
     if (!$formLength) {
-      alert('Please add some text to your tweet');
+      const excessWarning = 'Please add some text to your tweet';
+      const $warning = createWarning(excessWarning);
+
+      $lengthWarning.html($warning);
+      $lengthWarning.slideDown(1000);
       return;
     }
+
+    //Remove warning if visible and form input is valid
+    $lengthWarning.slideUp(1000);
 
     console.log('the form has submitted');
     // Gives us back urlencoded data
